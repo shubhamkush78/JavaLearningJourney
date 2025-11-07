6aac934766aff78b7dbdf83937c9758652f0e7fa
shubhamkush78 <shubhamkush780306@gmail.com>
2025-11-07 23:06:57 +0530

    string buffer builder

---
+++ 63_string_buffer_builder/A.java
@@ -0,0 +1,11 @@
+class A {
+    public static void main(String[] args) {
+        String a = "mohan";
+        String b = a;
+
+        a = a.toUpperCase();
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
